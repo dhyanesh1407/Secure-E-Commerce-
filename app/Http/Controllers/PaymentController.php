8a02526115709391cbae914e337b5420efa6fc19
dhyanesh1407 <135819230+dhyanesh1407@users.noreply.github.com>
2024-12-04 18:35:44 -0600

    Add files via upload

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+
+class PaymentController extends Controller
+{
+    public function createCheckoutSession(Request $request)
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' => [
+                            'name' => $request->tier,
+                        ],
+                        'unit_amount' => $request->amount, // Amount in cents
+                    ],
+                    'quantity' => 1,
+                ],
+            ],
+            'mode' => 'payment',
+            'success_url' => route('payment.success'),
+            'cancel_url' => route('payment.cancel'),
+        ]);
+
+        return response()->json(['id' => $session->id]);
+    }
+
+    public function success()
+    {
+        return view('home.payment');
+    }
+
+    public function cancel()
+    {
+        return view('home.cancel');
+    }
+}
